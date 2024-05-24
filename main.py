1b5180519e0b3adb018d3ad11a3d1579cd181c24
suraj kumar agasti <surajagasti92@gmail.com>
2024-05-24 16:18:43 +0530

    updated

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-# Desktop-Assistant-using-python
 
-# how to run this project
 
-1.create a new virtual environment.
-conda create -n assistant1 python=3.8
 
-2.Check the created virtual environment
-conda env
-
-3.Activate the virtual environment
-conda activate assistant
-
-4.Install all the packages present in the requirements
-pip install -r requirements.txt
 
 
